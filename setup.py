f90ffbfd1af57c99c0a16ab9e94717b3e9e3cc10
tkrabel <tobiaskrabel@gmx.de>
2019-06-06 20:05:21 +0200

    init

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'ppscore',
+  packages = ['ppscore'],
+  version = '0.0.0',
+  license='',
+  description = '',
+  author = 'Tobias Krabel, Florian Wetschoreck',
+  author_email = 'tobias@edaviz.com, florian@edaviz.com',
+  url = 'https://github.com/tkrabel/ppscore',
+  download_url = '',
+  keywords = [],
+  install_requires=[],
+  classifiers=[],
+)
